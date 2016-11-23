0c92303305761a29255e872901f2d1405c1258c4
olimonta <olimonta@hotmail.com>
2016-11-23 21:24:27 +0100

    Modification des fichiers index.html Weather.html controller.js et app.js
    Implementation du fichier ng-cordova.js dans index.html <script src="lib/ngCordova/dist/ng-cordova.min.js"></script>
    Modification apparence dans weather.html
    rajout [ng-cordova] dans app.js
    modification des appels aux variables lat et lon dans controllers.js

--- controllers.js
+++ controllers.js
@@ -1,18 +1,7 @@
 angular.module('weather')
 
 .controller('WeatherCtrl', function($scope, $cordovaGeolocation, $http, OpenWeatherConfig) {
-	$scope.geoloc = function(){
-	var posOptions = {timeout: 10000, enableHighAccuracy: false};
-		$cordovaGeolocation
-			.getCurrentPosition(posOptions)
-			.then(function (position) {
-				var lat = position.coords.latitude;
-				var lon = position.coords.longitude;
-				 console.log(lat + '' + lon)
-			}, function(err) {
-				// error
-			});
-	};
+	
 	
 	$scope.search = '';
     $scope.state = false;    
@@ -22,19 +11,33 @@ angular.module('weather')
     city: '',
     description: '',
     temp: '',
+	
 	  };
 	
-	
+	$scope.geoloc = function(){
+	var posOptions = {timeout: 10000, enableHighAccuracy: false};
+		$cordovaGeolocation
+			.getCurrentPosition(posOptions)
+			.then(function (position) {
+				var lat = position.coords.latitude;
+				var lon = position.coords.longitude;
+				 
+			}, function(err) {
+				// error
+			});
+	};
 	
     $scope.loadWeather = function(search, geoloc) {
 		
-            var url = OpenWeatherConfig.searchUrlLat + lat + OpenWeatherConfig.searchUrlLon + lon + OpenWeatherConfig.units + OpenWeatherConfig.appid;
+            var url = OpenWeatherConfig.searchUrlLat + geoloc.lat + OpenWeatherConfig.searchUrlLon + geoloc.lon + OpenWeatherConfig.units + OpenWeatherConfig.appid;
             $http.get(url).success(function(data) {
                 $scope.weatherData.icon = OpenWeatherConfig.imgUrl + data.weather[0].icon + '.png';
                 $scope.weatherData.main = data.weather[0].main;
                 $scope.weatherData.city = data.name;
                 $scope.weatherData.description = data.weather[0].description;
                 $scope.weatherData.temp = data.main.temp;
+				$scope.weatherData.lat = data.coord.lat;
+				$scope.weatherData.lon = data.coord.lon;
                 $scope.state = true;
             });
 		
